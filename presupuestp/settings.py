f62e1a0e053ea98a1828f37310e4bc5c09c2f834
adsuf <majobuitrago887@gmail.com>
2020-01-28 19:37:25 -0500

    movimientos

--- presupuestp/settings.py
+++ presupuestp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'billetera'
+    'billetera',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
